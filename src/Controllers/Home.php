88f39dfb2e382d35091cdf5dc22d2769f6368c0a
M1llengudgit <jsd_cnd@yahoo.fr>
2017-05-19 11:37:29 +0200

    vendor controllers src

--- src/Controllers/Home.php
+++ src/Controllers/Home.php
@@ -4,12 +4,19 @@ namespace Controllers;
 
 use Silex\Application;
 
+
 class Home{
 
     public function hello(Application $app, $name){
-        return $app['twig']->render('home/hello.html.twig',
+        return $app['twig']->render('hello.html.twig',
     array('name'=> $name));
 
     }
+
+    public function formulaire(Application $app){
+        return $app['twig']->render('pimpit.html.twig');
+
+    }
+
 }
  ?>
