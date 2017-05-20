8ed3111f1e83ccd54b5d4e935a18592e0a9c732a
didier-dev <didier.ferrand@free.fr>
2017-05-20 13:35:10 +0200

    didier - mon commentaire

--- src/Controllers/Home.php
+++ src/Controllers/Home.php
@@ -4,12 +4,7 @@ namespace Controllers;
 
 use Silex\Application;
 
-<<<<<<< HEAD
-class Home {
-=======
-
 class Home{
->>>>>>> 2d1cdd80dd5219fd01c235a022d5b658dff63549
 
     public function hello(Application $app, $name){
         return $app['twig']->render('hello.html.twig',
