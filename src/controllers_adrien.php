9c4efe57bf704d443ea46c90d13c943bb2947239
js-ddev <jsd_cnd@yahoo.fr>
2017-05-22 10:42:45 +0200

    Update controllers_adrien.php
    Modif retours à la ligne

--- src/controllers_adrien.php
+++ src/controllers_adrien.php
@@ -1,18 +1,15 @@
-<?php
-
-$app -> get('/pimpit/','Controllers\\Home::formulaire') -> bind('pimpit');
-
-$app -> get('/modeles/','Controllers\\Home::modeles') -> bind('modeles');
-
-$app -> get('/mentions_legales/','Controllers\\Home::mentions_legales') -> bind('mentions-legales');
-
-$app -> get('/contact/','Controllers\\Home::contact') -> bind('contact');
-
-$app -> get('/faq/','Controllers\\Home::faq') -> bind('faq');
-
-$app -> get('/about/','Controllers\\Home::about') -> bind('about');
-
-$app -> get('/inscription/','Controllers\\Home::inscription') -> bind('inscription');
-
-
-?>
+<?php
+
+$app -> get('/pimpit/','Controllers\\Home::formulaire') -> bind('pimpit');
+
+$app -> get('/modeles/','Controllers\\Home::modeles') -> bind('modeles');
+
+$app -> get('/mentions_legales/','Controllers\\Home::mentions_legales') -> bind('mentions-legales');
+
+$app -> get('/contact/','Controllers\\Home::contact') -> bind('contact');
+
+$app -> get('/faq/','Controllers\\Home::faq') -> bind('faq');
+
+$app -> get('/about/','Controllers\\Home::about') -> bind('about');
+
+$app -> get('/inscription/','Controllers\\Home::inscription') -> bind('inscription');
