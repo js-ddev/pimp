f2eb9307af0bb37a8cee74261c8ba05a67470c2f
js-ddev <jsd_cnd@yahoo.fr>
2017-05-22 10:40:15 +0200

    Update Home.php
    Modif des notes

--- src/Controllers/Home.php
+++ src/Controllers/Home.php
@@ -43,24 +43,3 @@ class Home
     }
 
 }
- ?>
-
-namespace Controllers;
-
-use Silex\Application;
-
-class Home{
-
-    public function hello(Application $app, $name){
-        return $app['twig']->render('hello.html.twig',
-    array('name'=> $name));
-
-    }
-
-    public function formulaire(Application $app){
-        return $app['twig']->render('pimpit.html.twig');
-
-    }
-
-}
- ?>
