a66c0444cf06b58e8b0f176e8408826b15a221cb
didier-dev <didier.ferrand@free.fr>
2017-06-01 11:48:31 +0200

    didier - BO cv sans boucle

--- web/js/plugins/morris/table.js
+++ web/js/plugins/morris/table.js
@@ -1,25 +1,30 @@
 $(function() {
  
-    var $this = $("#myTable");
-    var newrows = [];
-    $this.find("tr").each(function(){
-        var i = 0;
-        $(this).find("th").each(function(){
-            i++;
-            if(newrows[i] === undefined) { newrows[i] = $("<tr></tr>"); }
-            newrows[i].append($(this));
+    var $tables = $(".myTable");
+
+    $tables.each(function(){
+        $this = $(this);
+        var newrows = [];
+        $this.find("tr").each(function(){
+            var i = 0;
+            $(this).find("th").each(function(){
+                i++;
+                if(newrows[i] === undefined) { newrows[i] = $("<tr></tr>"); }
+                newrows[i].append($(this));
+            });
+            $(this).find("td").each(function(){
+                i++;
+                if(newrows[i] === undefined) { newrows[i] = $("<tr></tr>"); }
+                newrows[i].append($(this));
+            });
         });
-        $(this).find("td").each(function(){
-            i++;
-            if(newrows[i] === undefined) { newrows[i] = $("<tr></tr>"); }
-            newrows[i].append($(this));
+        $this.find("tr").remove();
+        $.each(newrows, function(){
+            $this.append(this);
         });
-    });
-    $this.find("tr").remove();
-    $.each(newrows, function(){
-        $this.append(this);
-    });
- 
-    $this.find("thead").html("<tr>"+$this.find("tr").html()+"</tr>");
-    $this.find("tbody tr").first().remove();
+     
+        $this.find("thead").html('<tr>'+$this.find("tr").html()+"</tr>");
+        $this.find("tbody tr").first().remove();
+        $this.fadeIn();
+    })
 });
